a6017b007c7b361ee10dc17e80cc3ffa10489cf3
relaxik <cs_vip@mail.ru>
2016-03-14 15:26:22 +1000

    Initial commit

---
+++ app/controllers/parser_controller.rb
@@ -0,0 +1,27 @@
+class ParserController < ApplicationController
+  # для получения контента через http
+  require 'open-uri'
+
+  # подключаем Nokogiri
+  require 'nokogiri'
+
+  # noinspection SpellCheckingInspection
+  def yandex
+    source = 'https://www.fl.ru/projects/'
+# + page
+    # получаем содержимое веб-страницы в объект
+    page = Nokogiri::HTML(open(source.to_s, 'User-Agent' => 'Opera'))
+    # производим поиск по элементам с помощью css-выборки
+    page.css('a.b-post__link').each do |link|
+
+      data = Hash.new
+
+      data['text'] = link.content
+
+      data['href'] = link['href']
+    end
+
+    return data
+
+  end
+end
