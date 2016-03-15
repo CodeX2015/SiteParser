f63200f8f3ff0e944f39daae05bcad699a1c0a6f
huligun <huligun@gmail.com>
2016-03-15 21:48:01 +1000

    model, controller for projects done

--- app/controllers/parser_controller.rb
+++ app/controllers/parser_controller.rb
@@ -8,46 +8,51 @@ class ParserController < ApplicationController
   # подключаем iconv
   # require 'iconv'
 
+  def clear_of_html_tags(str)
+    return ActionView::Base.full_sanitizer.sanitize(str)
+  end
+
   # noinspection SpellCheckingInspection
   def fl
     source = 'https://www.fl.ru/projects/'
 # + page
 # получаем содержимое веб-страницы в объект
     page = Nokogiri::HTML(open(source.to_s, 'User-Agent' => 'Opera'))
-    #@page_class = page.class
+    @page_class = page.class
 # производим поиск по элементам с помощью css-выборки
 
     page.css('div.b-post').each do |link|
 
-     #@link = link.text.to_s.force_encoding('cp1251') #.to_s.force_encoding('cp1251')
-      @link_title = link.css('a').text.to_s.force_encoding("cp1251")
-      @link_href = 'https://www.fl.ru' + link.css("a").map { |a| a['href'] }[0].to_s.force_encoding("cp1251")
+      #@link = link.text.to_s.force_encoding('cp1251') #.to_s.force_encoding('cp1251')
+
+      title = link.css('a').text.to_s
+
+      href = 'https://www.fl.ru' + link.css("a").map { |a| a['href'] }[0].to_s
+
       price = link.css('script')[0].text
-      price = price[price.index('(')+2..price.index(')')-2]
+      price = clear_of_html_tags(price[price.index('(')+2..price.index(')')-2].to_s)
+
       body = link.css('script')[1].text
-      body = body[body.index('(')+2..body.index(')')-2]
-      @link_price = ActionView::Base.full_sanitizer.sanitize(price).to_s.force_encoding("cp1251")
-      @link_body = ActionView::Base.full_sanitizer.sanitize(body).to_s.force_encoding("cp1251")
+      body = clear_of_html_tags(body[body.index('(')+2..body.index(')')-2].to_s)
+
       create_date = link.css('script')[2].text
-      create_date = create_date[create_date.index('(')+2..create_date.index(')')-2]
+      create_date = clear_of_html_tags(create_date[create_date.index('(')+2..create_date.index(')')-2].to_s)
 
-      @link_date = ActionView::Base.full_sanitizer.sanitize(create_date).to_s.force_encoding("cp1251")
 
-      Project.create(title: @link_title.force_encoding("cp1251"))
+      begin
+        Project.create(create_date: create_date, title: title, short_body: body, link: href, price: price)
+      rescue :ex
+        logger.error ex.message
+      end
 
+      @link_title = title.clone.force_encoding("cp1251")
+      @link_href = href.clone.force_encoding("cp1251")
+      @link_price = price.clone.force_encoding("cp1251")
+      @link_body = body.clone.force_encoding("cp1251")
+      @link_date = create_date.clone.force_encoding("cp1251")
 
       break
-
-
-      #data = Hash.new
-
-      #data['title'] = link[0].text
-
-      # data['href'] = link['href']
-      #puts(data[0].to_s)
     end
-
-    #return data
 
   end
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,3 @@
 class Project < ActiveRecord::Base
+  validates :link, :title, uniqueness: true, length: {minimum: 10}
 end
