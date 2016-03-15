eb09c75294431d75e76bdf40c4fd051b3357951b
relaxik <cs_vip@mail.ru>
2016-03-15 12:53:05 +1000

    #1 get price + href from html

--- app/controllers/parser_controller.rb
+++ app/controllers/parser_controller.rb
@@ -9,15 +9,26 @@ class ParserController < ApplicationController
   def fl
     source = 'https://www.fl.ru/projects/'
 # + page
-    # получаем содержимое веб-страницы в объект
-    page = Nokogiri::HTML(open(source.to_s, 'User-Agent' => 'Opera'))
+# получаем содержимое веб-страницы в объект
+    page = Nokogiri::HTML(open(source.to_s, 'User-Agent' => 'Opera', 'Accept-Charset' => 'Windows-1251'))
     @page_class = page.class
-    # производим поиск по элементам с помощью css-выборки
+# производим поиск по элементам с помощью css-выборки
+
     page.css('div.b-post').each do |link|
 
-      #@link = link
-      @link_title = link.css('h2 a').text
-      @link_price = link.css('script').text #.scan(/document.write\('\)/)
+      @link = link.to_s.force_encoding('cp1251') #.to_s.force_encoding('cp1251')
+      @link_title = link.css('a').text.to_s.force_encoding("cp1251")
+      @link_href = 'https://www.fl.ru' + link.css("a").map { |a| a['href'] }[0].to_s.force_encoding("cp1251")
+      #@link_price = link.css('div').text.to_s.force_encoding('cp1251')
+
+      @ssss = link.css('script')[0].text.to_s.force_encoding("cp1251") #.split(';')
+      @link_price = @ssss[@ssss.index('">')+2..@ssss.index('</')-2]
+
+      #@ssss.index('">')
+
+      #@ssss[@ssss.index('(') + 2.. @ssss.index(')')-2]
+      break
+
 
       #data = Hash.new
 
